acdfd618f41a2bcf14282d2f8c3a3c20e8d3fcbe
Kened Silva Felix <ksfelix1@stefanini.com>
2025-07-08 00:15:32 -0300

    training java

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,20 @@
 
 public class Principal {
 	public static void main(String[] args) {
-		int idade = 40 + 2;
-		System.out.println("Teste: " + idade);
+		Carro meuCarro;
+		
+		meuCarro = new Carro();
+		meuCarro.modelo = "Honda";
+		meuCarro.anoFabricacao = 1992;
+		
+		Carro seuCarro = new Carro();
+		seuCarro.modelo = "Fiat";
+		seuCarro.anoFabricacao = 2000;
+		
+		System.out.printf("Modelo : %s%n", meuCarro.modelo);
+		System.out.printf("Ano : %s%n", meuCarro.anoFabricacao);
+		
+		System.out.printf("Modelo : %s%n", seuCarro.modelo);
+		System.out.printf("Ano : %s%n", seuCarro.anoFabricacao);
 	}
 }
